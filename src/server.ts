f532979d41a5267ab9709f5a31813f18e605551e
drillprops <drylbartosz@gmail.com>
2019-12-23 21:35:35 +0100

    create server.ts

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { ApolloServer } from 'apollo-server';
+import resolvers from './graphql/resolvers';
+import typeDefs from './graphql/typeDefs';
+
+export const server = new ApolloServer({ typeDefs, resolvers });
