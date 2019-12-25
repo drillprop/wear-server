0fc2ee747dc48a3daefa4693e1508fb61a0f15e5
drillprops <drylbartosz@gmail.com>
2019-12-25 01:44:20 +0100

    create RegisterInput in typeDefs

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,13 +1,19 @@
 import { gql } from 'apollo-server';
 
 export default gql`
+  input RegisterInput {
+    email: String!
+    userName: String!
+    password: String!
+  }
   type Query {
     users: [User]
   }
   type Mutation {
-    createUser(email: String!, userName: String!, password: String!): User!
+    createUser(input: RegisterInput!): User!
   }
   type User {
+    token: String!
     id: ID!
     email: String!
     userName: String!
