56cd1454d6285b5674c094b148746e30ae049f20
drillprops <drylbartosz@gmail.com>
2020-01-04 16:06:52 +0100

    remove apollo server, install apollo server express with some dependencies instead

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from 'apollo-server-express';
+import 'dotenv/config';
+import express from 'express';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import resolvers from './graphql/resolvers';
+import typeDefs from './graphql/typeDefs';
+import { config } from './ormconfig';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const startServer = async () => {
+  const app = express();
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'http://localhost:3000'
+    })
+  );
+
+  app.use('/graphql', bodyParser.json(), cookieParser());
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => req
+  });
+
+  server.applyMiddleware({ app });
+
+  const connection = await createConnection(config);
+  await connection.runMigrations();
+
+  app.listen(process.env.PORT || 4000, () => {
+    console.log('server started');
+  });
+};
+
+startServer();
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/typeDefs';
-
-export const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => req
-});
