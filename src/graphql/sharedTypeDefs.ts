9b9f600fb455ad42c355c6b69fa59cdcbd9a0a45
drillprops <drylbartosz@gmail.com>
2020-01-09 01:51:12 +0100

    create more general SearchInput

--- src/graphql/sharedTypeDefs.ts
+++ src/graphql/sharedTypeDefs.ts
@@ -1,7 +1,23 @@
-import { ObjectType, Field } from 'type-graphql';
+import { ObjectType, Field, InputType, Int } from 'type-graphql';
 
 @ObjectType()
-export default class SuccessMessage {
+export class SuccessMessage {
   @Field()
   message: string;
+}
+
+@InputType()
+export class SearchInput {
+  @Field({ nullable: true })
+  column: string;
+  @Field({ nullable: true })
+  argument: string;
+  @Field(() => Int, { nullable: true })
+  take: number;
+  @Field(() => Int, { nullable: true })
+  skip: number;
+  @Field({ nullable: true })
+  orderBy: string;
+  @Field({ nullable: true })
+  desc: boolean;
 }
