f7fd778ee7ad42036d87caefc4a1539737c317ea
drillprops <drylbartosz@gmail.com>
2020-01-10 21:31:47 +0100

    add validation to SearchInput

--- src/graphql/shared/SearchInput.ts
+++ src/graphql/shared/SearchInput.ts
@@ -1,10 +1,13 @@
 import { InputType, Field, Int } from 'type-graphql';
+import { Length } from 'class-validator';
 
 @InputType()
 export default class SearchInput {
   @Field({ nullable: true })
+  @Length(1, 255)
   column: string;
   @Field({ nullable: true })
+  @Length(1, 255)
   argument: string;
   @Field(() => Int, { nullable: true })
   take: number;
