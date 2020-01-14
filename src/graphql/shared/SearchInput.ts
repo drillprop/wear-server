a47c720898e3b06300bd7448e6c674079a7bb57d
drillprops <drylbartosz@gmail.com>
2020-01-14 22:15:40 +0100

    add whereId field to SearchInput

--- src/graphql/shared/SearchInput.ts
+++ src/graphql/shared/SearchInput.ts
@@ -1,8 +1,10 @@
-import { InputType, Field, Int } from 'type-graphql';
+import { InputType, Field, Int, ID } from 'type-graphql';
 import { Length } from 'class-validator';
 
 @InputType()
 export default class SearchInput {
+  @Field(() => ID, { nullable: true })
+  whereId: string;
   @Field({ nullable: true })
   @Length(1, 255)
   column: string;
