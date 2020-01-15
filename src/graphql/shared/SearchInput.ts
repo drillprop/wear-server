e8788c394452ad79df5868b68b9cd30531b402ff
drillprops <drylbartosz@gmail.com>
2020-01-15 12:33:09 +0100

    remove column and argument in SearchInput

--- src/graphql/shared/SearchInput.ts
+++ src/graphql/shared/SearchInput.ts
@@ -1,16 +1,9 @@
 import { InputType, Field, Int, ID } from 'type-graphql';
-import { Length } from 'class-validator';
 
 @InputType()
 export default class SearchInput {
   @Field(() => ID, { nullable: true })
   whereId: string;
-  @Field({ nullable: true })
-  @Length(1, 255)
-  column: string;
-  @Field({ nullable: true })
-  @Length(1, 255)
-  argument: string;
   @Field(() => Int, { nullable: true })
   take: number;
   @Field(() => Int, { nullable: true })
