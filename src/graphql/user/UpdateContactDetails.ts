20254798be534aeb52885a782a043d3299b20723
drillprops <drylbartosz@gmail.com>
2020-01-22 10:34:35 +0100

    return SuccessMessage instead of user in updateContactDetails mutation

--- src/graphql/user/UpdateContactDetails.ts
+++ src/graphql/user/UpdateContactDetails.ts
@@ -2,11 +2,12 @@ import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import { User } from '../../entity/User';
 import { Context } from '../../types/context.types';
 import ContactDetailsInput from './updateContactDetails/ContactDetailsInput';
+import { SuccessMessage } from '../shared/SuccessMessage';
 
 @Resolver()
 export default class UpdateContactDetailsResolver {
   @Authorized(['ADMIN', 'EMPLOYEE', 'CUSTOMER'])
-  @Mutation(() => User)
+  @Mutation(() => SuccessMessage)
   async updateContactDetails(
     @Arg('input') input: ContactDetailsInput,
     @Ctx() { userId }: Context
@@ -14,8 +15,10 @@ export default class UpdateContactDetailsResolver {
     const userRepository = User.getRepository();
     try {
       const user = await User.findOne({ id: userId });
-      const updatedUser = await userRepository.save({ ...user, ...input });
-      return updatedUser;
+      await userRepository.save({ ...user, ...input });
+      return {
+        message: 'Succesfully updated contact details'
+      };
     } catch (error) {
       throw Error(error);
     }
