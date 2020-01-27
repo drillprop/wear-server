efb332e7846461f3d448101ef0451a1dd074e2f2
drillprops <drylbartosz@gmail.com>
2020-01-27 17:54:06 +0100

    create Address entity

---
+++ src/entity/Address.ts
@@ -0,0 +1,43 @@
+import { Field, ID, ObjectType } from 'type-graphql';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+import { User } from './User';
+
+@ObjectType()
+@Entity()
+export class Address extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  addresLine1: string;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  addresLine2: string;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  zipCode: string;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  city: string;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  country: string;
+
+  @OneToOne(
+    type => User,
+    user => user.address
+  )
+  user: User;
+}
