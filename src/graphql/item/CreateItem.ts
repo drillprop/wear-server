4979c967c662e25efe87c6770d5cdcf38e53df47
drillprops <drylbartosz@gmail.com>
2020-02-26 21:47:49 +0100

    update CreateItem and CreateItemInput to make possible setting sizes for item

--- src/graphql/item/CreateItem.ts
+++ src/graphql/item/CreateItem.ts
@@ -3,6 +3,7 @@ import { Item } from '../../entity/Item';
 import { User } from '../../entity/User';
 import { Context } from '../../types/context.types';
 import { CreateItemInput } from './createItem/CreateItemInput';
+import { Size } from '../../entity/Size';
 
 @Resolver()
 export default class CrateItemResolver {
@@ -13,10 +14,17 @@ export default class CrateItemResolver {
     @Ctx() { userId }: Context
   ) {
     try {
+      const { sizes, ...rest } = input;
       const user = await User.findOne({ id: userId });
       if (!user) throw Error('Something went wrong');
-      const item = Item.create({ ...input });
+
+      const item = Item.create({ ...rest });
+
+      const itemSizes = await Size.create({ ...sizes }).save();
+      item.sizes = Promise.resolve(itemSizes);
+
       item.createdBy = Promise.resolve(user);
+
       await item.save();
       return item;
     } catch (error) {
--- src/graphql/item/createItem/CreateItemInput.ts
+++ src/graphql/item/createItem/CreateItemInput.ts
@@ -1,7 +1,28 @@
-import { Field, InputType } from 'type-graphql';
+import { Field, InputType, Int } from 'type-graphql';
 import { Length, Min, IsUrl, MaxLength } from 'class-validator';
 import { IsNameNotTaken } from './IsNameNotTaken';
 import { Gender, Category } from '../../../entity/Item';
+
+@InputType()
+export class ItemSizes {
+  @Field(() => Int, { nullable: true })
+  xs: number;
+
+  @Field(() => Int, { nullable: true })
+  s: number;
+
+  @Field(() => Int, { nullable: true })
+  m: number;
+
+  @Field(() => Int, { nullable: true })
+  l: number;
+
+  @Field(() => Int, { nullable: true })
+  xl: number;
+
+  @Field(() => Int, { nullable: true })
+  xxl: number;
+}
 
 @InputType()
 export class CreateItemInput {
@@ -27,4 +48,7 @@ export class CreateItemInput {
   @Field({ nullable: true })
   @MaxLength(255)
   description: string;
+
+  @Field(() => ItemSizes, { nullable: true })
+  sizes: ItemSizes;
 }
