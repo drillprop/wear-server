81e6ea97ce934f911b2d40f456df12787dfe6fe4
drillprops <drylbartosz@gmail.com>
2020-02-28 14:34:34 +0100

    set DB_PORT and MAIL_PORT to string in env.types

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -2,12 +2,12 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     DB_NAME: string;
     DB_HOST: string;
-    DB_PORT: number;
+    DB_PORT: string;
     DB_PASSWORD: string;
     FRONTEND_URL: string;
     JWT_SECRET: string;
     MAIL_HOST: string;
-    MAIL_PORT: number;
+    MAIL_PORT: string;
     MAIL_USER: string;
     MAIL_PASS: string;
   }
--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  port: Number(process.env.MAIL_PORT),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
