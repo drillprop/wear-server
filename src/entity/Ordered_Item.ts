6319e45820e464cefac7c2d493b4efb7a1ca9a17
drillprops <drylbartosz@gmail.com>
2020-03-16 17:05:53 +0100

    create Ordered_Item entity

---
+++ src/entity/Ordered_Item.ts
@@ -0,0 +1,41 @@
+import { ObjectType, Field, ID, Int } from 'type-graphql';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne
+} from 'typeorm';
+import { SizeSymbol } from './Size';
+import { Order } from './Order';
+import { Item } from './Item';
+
+@ObjectType()
+@Entity()
+export class Ordered_Item extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @ManyToOne(
+    () => Order,
+    order => order.orderedItems,
+    { onDelete: 'CASCADE' }
+  )
+  order: Order;
+
+  @ManyToOne(
+    () => Item,
+    item => item.orderedItems,
+    { onDelete: 'CASCADE' }
+  )
+  item: Item;
+
+  @Column({
+    name: 'size_symbol',
+    type: 'enum',
+    enum: SizeSymbol
+  })
+  @Field(() => SizeSymbol)
+  sizeSymbol: SizeSymbol;
+}
