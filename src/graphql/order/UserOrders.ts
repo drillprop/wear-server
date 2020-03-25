9dab9565b9c22ff01e62c3243cabb744da3da3cf
drillprops <drylbartosz@gmail.com>
2020-03-25 20:30:40 +0100

    add select and count to UserOrder resolver, wrap code in try catch statement

--- src/graphql/order/UserOrders.ts
+++ src/graphql/order/UserOrders.ts
@@ -1,23 +1,31 @@
-import { Query, Resolver, Ctx, Arg } from 'type-graphql';
+import { Arg, Ctx, ObjectType, Query, Resolver } from 'type-graphql';
+import { Order } from '../../entity/Order';
 import { User } from '../../entity/User';
 import { Context } from '../../types/context.types';
-import { Order } from '../../entity/Order';
+import SelectAndCount from '../shared/SelectAndCount';
+
+@ObjectType()
+class UserOrdersAndCount extends SelectAndCount(Order) {}
 
 @Resolver()
 export class UserOrders {
-  @Query(() => [Order], { nullable: true })
+  @Query(() => UserOrdersAndCount, { nullable: true })
   async userOrders(
     @Arg('take', { defaultValue: 5 }) take: number,
     @Arg('skip') skip: number,
     @Ctx() { userId }: Context
   ) {
-    const user = await User.findOne({ id: userId });
-    if (!user) throw Error('You must be logged in');
-    const orders = await Order.find({
-      where: `ordered_by = '${user.id}'`,
-      take,
-      skip
-    });
-    return orders;
+    try {
+      const user = await User.findOne({ id: userId });
+      if (!user) throw Error('You must be logged in');
+      const [select, count] = await Order.findAndCount({
+        where: `ordered_by = '${user.id}'`,
+        take,
+        skip
+      });
+      return { select, count };
+    } catch (error) {
+      throw Error(error);
+    }
   }
 }
